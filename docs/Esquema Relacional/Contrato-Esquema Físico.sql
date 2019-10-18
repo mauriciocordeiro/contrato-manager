8913a64134b379f145482e1589e1baee3205c373
dias1618 <dias.1618@gmail.com>
2019-10-18 09:36:16 -0300

    Atualização na população do modelo relacional

--- docs/Esquema Relacional/Contrato-Esquema Físico.sql
+++ docs/Esquema Relacional/Contrato-Esquema Físico.sql
@@ -6,7 +6,7 @@
 CREATE TABLE Setor (
 id_setor INTEGER PRIMARY KEY,
 nome VARCHAR(50)
-)
+);
 
 CREATE TABLE Usuario (
 id_usuario INTEGER PRIMARY KEY,
@@ -15,27 +15,27 @@ senha VARCHAR(20),
 nome VARCHAR(50),
 login VARCHAR(20),
 FOREIGN KEY(id_setor) REFERENCES Setor (id_setor)
-)
+);
 
 CREATE TABLE Aditivo (
 id_aditivo INTEGER,
 id_contrato INTEGER,
 id_empresa INTEGER,
 copia_contrato_aditivo VARCHAR(100),
-valor_contrato_aditivo DECIMAL(2),
+valor_contrato_aditivo DECIMAL,
 observacoes VARCHAR(500),
-data_renovacao DATETIME,
-data_vencimento DATETIME,
+data_renovacao TIMESTAMP,
+data_vencimento TIMESTAMP,
 PRIMARY KEY(id_aditivo,id_contrato,id_empresa)
-)
+);
 
 CREATE TABLE Empresa (
 id_empresa INTEGER PRIMARY KEY,
 cnpj VARCHAR(14),
-razao_social VARCHAR(50),
+razao_social VARCHAR(200),
 email VARCHAR(50),
 nome_fantasia VARCHAR(50)
-)
+);
 
 CREATE TABLE Telefone (
 id_telefone INTEGER,
@@ -44,7 +44,7 @@ numero VARCHAR(15),
 ddd VARCHAR(2),
 PRIMARY KEY(id_telefone,id_empresa),
 FOREIGN KEY(id_empresa) REFERENCES Empresa (id_empresa)
-)
+);
 
 CREATE TABLE Endereco (
 id_endereco INTEGER,
@@ -56,15 +56,15 @@ cidade VARCHAR(50),
 uf VARCHAR(2),
 PRIMARY KEY(id_endereco,id_empresa),
 FOREIGN KEY(id_empresa) REFERENCES Empresa (id_empresa)
-)
+);
 
 CREATE TABLE Contrato (
 id_contrato INTEGER,
 id_empresa INTEGER,
 copia_contrato VARCHAR(100),
-data_finalizacao DATETIME,
-data_celebracao DATETIME,
-valor_contrato DECIMAL(2),
+data_finalizacao TIMESTAMP,
+data_celebracao TIMESTAMP,
+valor_contrato DECIMAL,
 prestacao INTEGER,
 tipo_contrato INTEGER,
 status_contrato INTEGER,
@@ -72,7 +72,7 @@ numero VARCHAR(50),
 observacoes VARCHAR(500),
 PRIMARY KEY(id_contrato,id_empresa),
 FOREIGN KEY(id_empresa) REFERENCES Empresa (id_empresa)
-)
+);
 
 CREATE TABLE Conta (
 id_conta INTEGER,
@@ -81,25 +81,25 @@ id_empresa INTEGER,
 copia_conta VARCHAR(100),
 status_conta INTEGER,
 observacoes VARCHAR(500),
-data_envio_dof DATETIME,
-data_vencimento DATETIME,
-data_recebimento_setor DATETIME,
-valor_conta DECIMAL(2),
+data_envio_dof TIMESTAMP,
+data_vencimento TIMESTAMP,
+data_recebimento_setor TIMESTAMP,
+valor_conta DECIMAL,
 tipo_conta INTEGER,
 PRIMARY KEY(id_conta,id_contrato,id_empresa),
 FOREIGN KEY(id_contrato, id_empresa) REFERENCES Contrato (id_contrato,id_empresa)
-)
+);
 
 CREATE TABLE Pagamento (
-id_pagamento VARCHAR(50),
+id_pagamento INTEGER,
 id_conta INTEGER,
 id_contrato INTEGER,
 id_empresa INTEGER,
-valor_pago VARCHAR(50),
-taxa_juros DECIMAL(2),
-data_pagamento_conta DATETIME,
+valor_pago DECIMAL,
+taxa_juros DECIMAL,
+data_pagamento_conta TIMESTAMP,
 PRIMARY KEY(id_pagamento,id_conta,id_contrato,id_empresa),
 FOREIGN KEY(id_conta, id_contrato, id_empresa) REFERENCES Conta (id_conta,id_contrato,id_empresa)
-)
+);
 
-ALTER TABLE Aditivo ADD FOREIGN KEY(id_contrato, id_empresa) REFERENCES Contrato (id_contrato,id_empresa)
+ALTER TABLE Aditivo ADD FOREIGN KEY(id_contrato, id_empresa) REFERENCES Contrato (id_contrato,id_empresa);
--- docs/Esquema Relacional/Contrato-insert.sql
+++ docs/Esquema Relacional/Contrato-insert.sql
@@ -11,7 +11,7 @@ INSERT INTO Usuario (id_usuario, nome, login, senha, id_setor)
 	VALUES (2, 'Ezequiel', 'ezequias', '101010', 2);
 	
 -- INSERÇÃO DE DADOS NA TABELA EMPRESA
-INSERT INTO Empresa (id_empresa, razao_social, nome_fantasia, email)
+INSERT INTO Empresa (id_empresa, cnpj, razao_social, nome_fantasia, email)
 	VALUES (1, '11111111111111', 'Coelba Limitada', 'Coelba Conquista', 'coelba@provedor.com');
 INSERT INTO Endereco(id_endereco, rua, numero, bairro, cidade, uf, id_empresa)
 	VALUES (1, 'Olivia Flores', 100, 'Candeias', 'Vitoria da Conquista', 'BA', 1);
@@ -21,82 +21,82 @@ INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (2, 1, '77', '11111111');
 	
 	
-INSERT INTO Empresa (id_empresa, razao_social, nome_fantasia, email)
+INSERT INTO Empresa (id_empresa, cnpj, razao_social, nome_fantasia, email)
 	VALUES (2, '22222222222222', 'Embasa Limitada', 'Embasa Conquista', 'embasa@provedor.com');
 INSERT INTO Endereco(id_endereco, rua, numero, bairro, cidade, uf, id_empresa)
 	VALUES (1, 'Para', 200, 'Ibirapuera', 'Vitoria da Conquista', 'BA', 2);
 INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (1, 2, '77', '22222222');
-INSERT INTO Telefone (id_telefone, ddd, telefone)
+INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (2, 2, '74', '568745129');
 
-INSERT INTO Empresa (id_empresa, razao_social, nome_fantasia, email)
+INSERT INTO Empresa (id_empresa, cnpj, razao_social, nome_fantasia, email)
 	VALUES (3, '33333333333333', 'Lanchonete Teixeira', 'Lanchonete Teixeira', 'teixeirao@provedor.com');
 INSERT INTO Endereco(id_endereco, rua, numero, bairro, cidade, uf, id_empresa)
 	VALUES (1, 'Siqueira Campos', 300, 'Centro', 'Vitoria da Conquista', 'BA', 3);
-INSERT INTO Telefone (id_telefone, ddd, telefone)
+INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (1, 3, '73', '264512598');
 
 
-INSERT INTO Empresa (id_empresa, razao_social, nome_fantasia, email)
+INSERT INTO Empresa (id_empresa, cnpj, razao_social, nome_fantasia, email)
 	VALUES (4, '44444444444444', 'Seguro Alunos RSA', 'Seguro', 'segurorsa@provedor.com');
 INSERT INTO Endereco(id_endereco, rua, numero, bairro, cidade, uf, id_empresa)
 	VALUES (1, 'Nove de Novembro', 400, 'Guarani', 'Tapirema', 'BA', 4);
-INSERT INTO Telefone (id_telefone, ddd, telefone)
+INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (1, 4, '75', '65217839');
 
 
-INSERT INTO Empresa (id_empresa, razao_social, nome_fantasia, email)
+INSERT INTO Empresa (id_empresa, cnpj, razao_social, nome_fantasia, email)
 	VALUES (5, '55555555555555', 'Coelba Limitada', 'Coelba Tapirema', 'coelbatapirema@provedor.com');
 INSERT INTO Endereco(id_endereco, rua, numero, bairro, cidade, uf, id_empresa)
 	VALUES (1, 'Sete de Setembro', 500, 'Guarani', 'Tapirema', 'BA', 5);	
-INSERT INTO Telefone (id_telefone, ddd, telefone)
+INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (1, 5, '77', '45657894');	
 		
 
-INSERT INTO Empresa (id_empresa, razao_social, nome_fantasia, email)
+INSERT INTO Empresa (id_empresa, cnpj, razao_social, nome_fantasia, email)
 	VALUES (6, '66666666666666', 'Contrulita Prestadora de Serviços em Construções Empresariais SA', 'Construlita', 'constulita@provedor.com');
 INSERT INTO Endereco(id_endereco, rua, numero, bairro, cidade, uf, id_empresa)
 	VALUES (1, 'Luis Eduardo', 600, 'Jurema', 'Vitoria da Conquista', 'BA', 6);	
-INSERT INTO Telefone (id_telefone, ddd, telefone)
+INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (1, 6, '71', '888888884');
 	
 
-INSERT INTO Empresa (id_empresa, razao_social, nome_fantasia, email)
+INSERT INTO Empresa (id_empresa, cnpj, razao_social, nome_fantasia, email)
 	VALUES (7, '77777777777777', 'Companhia Telefonica', 'Telefone', 'oi@provedor.com');
 INSERT INTO Endereco(id_endereco, rua, numero, bairro, cidade, uf, id_empresa)
 	VALUES (1, 'Brasil', 700, 'Zabele', 'Vitoria da Conquista', 'BA', 7);		
-INSERT INTO Telefone (id_telefone, ddd, telefone)
+INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (1, 7, '75', '88888888');
-INSERT INTO Telefone (id_telefone, ddd, telefone)
+INSERT INTO Telefone(id_telefone, id_empresa, ddd, numero)
 	VALUES (2, 7, '77', '999999999');
 	
 	
 -- INSERÇÃO DE DADOS NA TABELA CONTRATO
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (1, 1, 'C:\copia100.pdf', 'Licitatorio', 'Servico', 'Ativo', 12357.00, '2013-11-02', '2014-11-02', NULL, '12345');
+	VALUES (1, 1, 'C:\copia100.pdf', 1, 1, 1, 12357.00, '2013-11-02', '2014-11-02', NULL, '12345');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (1, 2, 'C:\copia200.pdf', 'Licitatorio', 'Servico', 'Ativo',	8570.00, '2012-12-01', '2013-12-01', 'Reclamações por parte dos Alunos do Curso dde BSI no dia  23/10/2014', '45623');
+	VALUES (1, 2, 'C:\copia200.pdf', 1, 1, 1,	8570.00, '2012-12-01', '2013-12-01', 'Reclamações por parte dos Alunos do Curso dde BSI no dia  23/10/2014', '45623');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (1, 3, 'C:\copia50.pdf', 'Licitatorio', 'Servico', 'Finalizado', 17400.00, '2011-05-16', '2012-05-06', NULL, '83452');
+	VALUES (1, 3, 'C:\copia50.pdf', 1, 1, 2, 17400.00, '2011-05-16', '2012-05-06', NULL, '83452');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (2, 3, 'C:\copia3000.pdf', 'Licitatorio', 'Produto', 'Finalizado', NULL, '2014-10-05', '2015-10-05', NULL, '83462');
+	VALUES (2, 3, 'C:\copia3000.pdf', 1, 2, 2, NULL, '2014-10-05', '2015-10-05', NULL, '83462');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (1, 4, 'C:\copia300.pdf', 'Permanente', 'Servico', 'Ativo', NULL, '2000-11-15', NULL, NULL, '23742');
+	VALUES (1, 4, 'C:\copia300.pdf', 2, 1, 1, NULL, '2000-11-15', NULL, NULL, '23742');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (2, 4, 'C:\copia400.pdf', 'Licitatorio', 'Servico', 'Cancelado', 13985.00, '2012-12-01',	'2013-12-01', NULL, '84563');
+	VALUES (2, 4, 'C:\copia400.pdf', 1, 1, 3, 13985.00, '2012-12-01',	'2013-12-01', NULL, '84563');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (3, 4, 'C:\copia5000.pdf', 'Permanente', 'Servico', 'Ativo', NULL, '2001-05-20', NULL, NULL, '02352');
+	VALUES (3, 4, 'C:\copia5000.pdf', 2, 1, 1, NULL, '2001-05-20', NULL, NULL, '02352');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (1, 5, 'C:\copia123.pdf', 'Permanente', 'Servico', 'Ativo', NULL, '2000-10-21', NULL, NULL, '83562');
+	VALUES (1, 5, 'C:\copia123.pdf', 2, 1, 1, NULL, '2000-10-21', NULL, NULL, '83562');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (2, 5, 'C:\copia555.pdf', 'Construtora', 'Servico', 'Ativo', 75890.00, '2012-09-21', NULL, 'Houve risco de embargo na visita do Orgão fiscalizador por conta da falta de EPIs e EPCs para os funcionarios. Solucionado pela empresa.', '57358');
+	VALUES (2, 5, 'C:\copia555.pdf', 3, 1, 1, 75890.00, '2012-09-21', NULL, 'Houve risco de embargo na visita do Orgão fiscalizador por conta da falta de EPIs e EPCs para os funcionarios. Solucionado pela empresa.', '57358');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (1, 6, 'C:\copia789.pdf', 'Permanente', 'Servico', 'Finalizado', NULL, '2002-05-08', NULL, NULL, '24724');
+	VALUES (1, 6, 'C:\copia789.pdf', 2, 1, 2, NULL, '2002-05-08', NULL, NULL, '24724');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (1, 7, 'C:\copia55.pdf', 'Licitatorio', 'Servico', 'Ativo', 20000.00, '2012-04-10', '2013-04-10', NULL, '67347');
+	VALUES (1, 7, 'C:\copia55.pdf', 1, 1, 1, 20000.00, '2012-04-10', '2013-04-10', NULL, '67347');
 INSERT INTO Contrato (id_contrato, id_empresa, copia_contrato, tipo_contrato, prestacao, status_contrato, valor_contrato, data_celebracao, data_finalizacao, observacoes, numero)
-	VALUES (2, 7, 'C:\copia234.pdf', 'Licitatorio', 'Servico', 'Ativo', 34580.00, '2012-02-03', '2013-02-03', NULL, '34622');
+	VALUES (2, 7, 'C:\copia234.pdf', 1, 1, 1, 34580.00, '2012-02-03', '2013-02-03', NULL, '34622');
 
 -- INSERÇÃO DE DADOS NA TABELA ADITIVO
 INSERT INTO Aditivo (id_aditivo, id_contrato, id_empresa, copia_contrato_aditivo, valor_contrato_aditivo, data_renovacao, data_vencimento, observacoes)
@@ -121,46 +121,46 @@ INSERT INTO Aditivo (id_aditivo, id_contrato, id_empresa, copia_contrato_aditivo
 	VALUES (1, 2, 7, 'C:\copia23401.pdf', 42345.00, '2013-02-04', '2014-02-04', 'Ampliou o quadro de fincionários em 10 pessoas');
 
 -- INSERÇÃO DE DADOS NA TABELA CONTA e PAGAMENTO
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (1, 1, 1, 'C:\copia300_01.pdf', 'Despesa', 'Pago', '2014-05-06', '2014-05-02', '2014-05-02', 12987.00, NULL, 2);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (1, 1, 1, 'C:\copia300_01.pdf', 2, 1, '2014-05-06', '2014-05-02', '2014-05-02', 12987.00, NULL);
 INSERT INTO PAGAMENTO(id_pagamento, id_conta, id_contrato, id_empresa, valor_pago, taxa_juros, data_pagamento_conta)	
-	VALUES(1, 1, 1, 1, 12987.00, 0, '2014-05-04')
+	VALUES(1, 1, 1, 1, 12987.00, 0, '2014-05-04');
 	
 	
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (1, 1, 4, 300, 'C:\copia300_02.pdf', 'Despesa', 'Pago', '2014-06-06', '2014-06-05', '2014-06-05', 15321.00, NULL, 2);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (1, 1, 4, 'C:\copia300_02.pdf', 2, 1, '2014-06-06', '2014-06-05', '2014-06-05', 15321.00, NULL);
 INSERT INTO PAGAMENTO(id_pagamento, id_conta, id_contrato, id_empresa, valor_pago, taxa_juros, data_pagamento_conta)	
-	VALUES(1, 1, 1, 4, 15321.00, 0, '2014-06-06')	
+	VALUES(1, 1, 1, 4, 15321.00, 0, '2014-06-06');	
 	
 	
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (2, 1, 4, 5000, 'C:\copia5000_01.pdf', 'Despesa', 'Pago', '2014-05-10', '2014-05-05', '2014-05-06', 9343.00, NULL, 2);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (2, 1, 4, 'C:\copia5000_01.pdf', 2, 1, '2014-05-10', '2014-05-05', '2014-05-06', 9343.00, NULL);
 INSERT INTO PAGAMENTO(id_pagamento, id_conta, id_contrato, id_empresa, valor_pago, taxa_juros, data_pagamento_conta)	
-	VALUES(1, 2, 1, 4, 9343.00, 0, '2014-05-08')		
+	VALUES(1, 2, 1, 4, 9343.00, 0, '2014-05-08');		
 	
 	
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (1, 3, 4, 'C:\copia5000_02.pdf', 'Despesa', 'Pago', '2014-06-10', '2014-06-09', '2014-06-09', 8743.00, NULL, 2);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (1, 3, 4, 'C:\copia5000_02.pdf', 2, 1, '2014-06-10', '2014-06-09', '2014-06-09', 8743.00, NULL);
 INSERT INTO PAGAMENTO(id_pagamento, id_conta, id_contrato, id_empresa, valor_pago, taxa_juros, data_pagamento_conta)	
-	VALUES(1, 1, 3, 4, 8743.00, 0, '2014-06-10')			
+	VALUES(1, 1, 3, 4, 8743.00, 0, '2014-06-10');			
 	
 	
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (2, 3, 4, 5000, 'C:\copia5000_03.pdf', 'Despesa', 'Em Aberto', '2014-07-10', '2014-07-01', '2014-07-01', 10286.00, NULL, 1);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (2, 3, 4, 'C:\copia5000_03.pdf', 2, 2, '2014-07-10', '2014-07-01', '2014-07-01', 10286.00, NULL);
 
 
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (1, 2, 5, 'C:\copia200_01.pdf', 'Receita', 'Pago', '2014-06-11', '2014-06-07', '2014-06-07', 760.00, NULL, 2);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (1, 2, 5, 'C:\copia200_01.pdf', 1, 1, '2014-06-11', '2014-06-07', '2014-06-07', 760.00, NULL);
 INSERT INTO PAGAMENTO(id_pagamento, id_conta, id_contrato, id_empresa, valor_pago, taxa_juros, data_pagamento_conta)	
-	VALUES(1, 1, 2, 5, 760.00, 0, '2014-06-09')				
+	VALUES(1, 1, 2, 5, 760.00, 0, '2014-06-09');				
 
 	
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (1, 1, 6, 'C:\copia200_02.pdf', 'Receita', 'Em Aberto', '2014-07-11', '2014-07-07', NULL, 760.00, NULL, 1);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (1, 1, 6, 'C:\copia200_02.pdf', 1, 2, '2014-07-11', '2014-07-07', NULL, 760.00, NULL);
 
 	
-INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento_conta, data_recebimento_setor, data_envio_dof, valor_conta, observacoes_conta, id_usuario)
-	VALUES (2, 1, 6, 'C:\copia300_03.pdf', 'Despesa', 'Pago', '2014-07-06', '2014-07-01', '2014-07-04', 11570.00, NULL, 1);
+INSERT INTO CONTA (id_conta, id_contrato, id_empresa, copia_conta, tipo_conta, status_conta, data_vencimento, data_recebimento_setor, data_envio_dof, valor_conta, observacoes)
+	VALUES (2, 1, 6, 'C:\copia300_03.pdf', 2, 1, '2014-07-06', '2014-07-01', '2014-07-04', 11570.00, NULL);
 INSERT INTO PAGAMENTO(id_pagamento, id_conta, id_contrato, id_empresa, valor_pago, taxa_juros, data_pagamento_conta)	
-	VALUES(1, 2, 1, 6, 12527.00, 10, '2014-07-10')					
+	VALUES(1, 2, 1, 6, 12527.00, 10, '2014-07-10');					
 	
