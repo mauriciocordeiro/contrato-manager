fe5a9a790c64326f31f640123c773d15e8abc32a
Itallo Silva <itallosilva@yahoo.com.br>
2019-10-19 14:32:16 -0300

    Coleção setor

---
+++ docs/Esquema Não Relacional/colecao-setor.js
@@ -0,0 +1,13 @@
+// ======================
+// Banco de Dados CONTRATO
+// ======================
+
+// Cria o banco de dados empresa
+use contrato
+
+// Remove todos os documentos da coleção empresa
+db.setor.remove({});
+
+// Insere um documento na coleção empresa
+db.setor.insertOne({_id:NumberInt(1),nome:"Fiscal", "usuarios":[{nome:"Cassio", login:"cassio", senha:"12345"}]});
+db.setor.insertOne({_id:NumberInt(2),nome:"Financeiro", "usuarios":[{nome:"Ezequiel", login:"ezequias", senha:"101010"}]});
