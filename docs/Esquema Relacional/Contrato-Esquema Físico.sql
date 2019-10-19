09b152a49c99ec0540ccbce4d4c1ab8ac4489b9d
dias1618 <dias.1618@gmail.com>
2019-10-18 21:28:00 -0300

    Modificações sugeridas pelo professor

--- docs/Esquema Relacional/Contrato-Esquema Físico.sql
+++ docs/Esquema Relacional/Contrato-Esquema Físico.sql
@@ -6,7 +6,7 @@
 CREATE TABLE Setor (
 id_setor INTEGER PRIMARY KEY,
 nome VARCHAR(50)
-);
+)
 
 CREATE TABLE Usuario (
 id_usuario INTEGER PRIMARY KEY,
@@ -15,7 +15,7 @@ senha VARCHAR(20),
 nome VARCHAR(50),
 login VARCHAR(20),
 FOREIGN KEY(id_setor) REFERENCES Setor (id_setor)
-);
+)
 
 CREATE TABLE Aditivo (
 id_aditivo INTEGER,
@@ -23,19 +23,19 @@ id_contrato INTEGER,
 id_empresa INTEGER,
 copia_contrato_aditivo VARCHAR(100),
 valor_contrato_aditivo DECIMAL,
-observacoes VARCHAR(500),
-data_renovacao TIMESTAMP,
-data_vencimento TIMESTAMP,
+observacoes VARCHAR(255),
+data_renovacao DATETIME,
+data_vencimento DATETIME,
 PRIMARY KEY(id_aditivo,id_contrato,id_empresa)
-);
+)
 
 CREATE TABLE Empresa (
 id_empresa INTEGER PRIMARY KEY,
 cnpj VARCHAR(14),
-razao_social VARCHAR(200),
+razao_social VARCHAR(50),
 email VARCHAR(50),
 nome_fantasia VARCHAR(50)
-);
+)
 
 CREATE TABLE Telefone (
 id_telefone INTEGER,
@@ -44,7 +44,7 @@ numero VARCHAR(15),
 ddd VARCHAR(2),
 PRIMARY KEY(id_telefone,id_empresa),
 FOREIGN KEY(id_empresa) REFERENCES Empresa (id_empresa)
-);
+)
 
 CREATE TABLE Endereco (
 id_endereco INTEGER,
@@ -56,23 +56,34 @@ cidade VARCHAR(50),
 uf VARCHAR(2),
 PRIMARY KEY(id_endereco,id_empresa),
 FOREIGN KEY(id_empresa) REFERENCES Empresa (id_empresa)
-);
+)
 
 CREATE TABLE Contrato (
 id_contrato INTEGER,
 id_empresa INTEGER,
 copia_contrato VARCHAR(100),
-data_finalizacao TIMESTAMP,
-data_celebracao TIMESTAMP,
+data_finalizacao DATETIME,
+data_celebracao DATETIME,
 valor_contrato DECIMAL,
 prestacao INTEGER,
 tipo_contrato INTEGER,
 status_contrato INTEGER,
 numero VARCHAR(50),
-observacoes VARCHAR(500),
+observacoes VARCHAR(255),
 PRIMARY KEY(id_contrato,id_empresa),
 FOREIGN KEY(id_empresa) REFERENCES Empresa (id_empresa)
-);
+)
+
+CREATE TABLE Pagamento (
+id_pagamento VARCHAR(50),
+id_conta INTEGER,
+id_contrato INTEGER,
+id_empresa INTEGER,
+valor_pago VARCHAR(50),
+taxa_juros DECIMAL,
+data_pagamento_conta DATETIME,
+PRIMARY KEY(id_pagamento,id_conta,id_contrato,id_empresa)
+)
 
 CREATE TABLE Conta (
 id_conta INTEGER,
@@ -80,26 +91,15 @@ id_contrato INTEGER,
 id_empresa INTEGER,
 copia_conta VARCHAR(100),
 status_conta INTEGER,
-observacoes VARCHAR(500),
-data_envio_dof TIMESTAMP,
-data_vencimento TIMESTAMP,
-data_recebimento_setor TIMESTAMP,
+observacoes VARCHAR(255),
+data_envio_dof DATETIME,
+data_vencimento DATETIME,
+data_recebimento_setor DATETIME,
 valor_conta DECIMAL,
 tipo_conta INTEGER,
 PRIMARY KEY(id_conta,id_contrato,id_empresa),
 FOREIGN KEY(id_contrato, id_empresa) REFERENCES Contrato (id_contrato,id_empresa)
-);
+)
 
-CREATE TABLE Pagamento (
-id_pagamento INTEGER,
-id_conta INTEGER,
-id_contrato INTEGER,
-id_empresa INTEGER,
-valor_pago DECIMAL,
-taxa_juros DECIMAL,
-data_pagamento_conta TIMESTAMP,
-PRIMARY KEY(id_pagamento,id_conta,id_contrato,id_empresa),
-FOREIGN KEY(id_conta, id_contrato, id_empresa) REFERENCES Conta (id_conta,id_contrato,id_empresa)
-);
-
-ALTER TABLE Aditivo ADD FOREIGN KEY(id_contrato, id_empresa) REFERENCES Contrato (id_contrato,id_empresa);
+ALTER TABLE Aditivo ADD FOREIGN KEY(id_contrato, id_empresa) REFERENCES Contrato (id_contrato,id_empresa)
+ALTER TABLE Pagamento ADD FOREIGN KEY(id_conta, id_contrato, id_empresa) REFERENCES Conta (id_conta,id_contrato,id_empresa)
